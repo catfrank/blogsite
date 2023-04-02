70fe0c6cd36a6b171e199fc6d54f6f0ca8569152
catfrank <catfrank1228@gmail.com>
2023-04-02 16:20:45 +0800

    0402 页面版式

--- blog/urls.py
+++ blog/urls.py
@@ -4,4 +4,8 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('programs/', views.programs, name='programs'),
+    path('writings/', views.writings, name='writings'),
+    path('words/', views.words, name='words'),
+    path('sentences/', views.sentences, name='sentences'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponse
 
 from .models import Articles, Tags, Words, Sentences
 
-
+# 主页视图
 def index(request):
     articles = Articles.objects.order_by('-created')
     context = {
@@ -14,4 +14,34 @@ def index(request):
     }
     return render(request, 'index.html', context)
 
-    
+# Programs视图
+def programs(request):
+    programs = Articles.objects.filter(article_type_id = 1)
+    context = {
+        'programs': programs,
+    }
+    return render(request, 'programs.html', context)
+
+# Writings视图
+def writings(request):
+    writings = Articles.objects.filter(article_type_id = 2)
+    context = {
+        'writings': writings,
+    }
+    return render(request, 'writings.html', context)
+
+# Words视图
+def words(request):
+    words = Words.objects.all()
+    context = {
+        'words': words,
+    }
+    return render(request, 'words.html', context)
+
+# Sentences视图
+def sentences(request):
+    sentences = Sentences.objects.all()
+    context = {
+        'sentences': sentences,
+    }
+    return render(request, 'sentences.html', context)
